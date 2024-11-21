19ad399ad70de22c74e648e17d8bc8bc7d7a8353
ferza <ferzan17@gmail.com>
2024-11-20 22:38:49 -0300

    Cleaning the whole structure

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,17 +1,18 @@
 import grpc
-from proto.pb2 import user_pb2, users_pb2_grpc
+from controllers.proto.pb2.users_pb2 import UserResponse, UserResponse
+from controllers.proto.pb2.users_pb2_grpc import UserServiceServicer
 from components.component import fetch_user
 
 
-class UserController(users_pb2_grpc.UsersServicer):
+class UserController(UserServiceServicer):
     def GetUser(self, request, context):
         user_id = request.id
         response = fetch_user(user_id)
         if not response:
             context.set_details("User not found")
             context.set_code(grpc.StatusCode.NOT_FOUND)
-            return user_pb2.UserResponse()
-        return user_pb2.UserResponse(
+            return UserResponse()
+        return UserResponse(
             id=response["id"],
             name=response["name"],
             trx_id=response["trx_id"]
--- app/main.py
+++ app/main.py
@@ -9,13 +9,11 @@ from controllers.proto.pb2 import users_pb2_grpc
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    users_pb2_grpc.add_UsersServicer_to_server(UserController(), server)
+    users_pb2_grpc.add_UserServiceServicer_to_server(UserController(), server)
     server.add_insecure_port('[::]:50051')
-    print("Starting server on port 50051...")
+    print("Started gRPC Server on port 50051")
     server.start()
     server.wait_for_termination()
 
 if __name__ == '__main__':
-    logging.basicConfig()
-    print("Starting server in: localhost:50051")
     serve()
